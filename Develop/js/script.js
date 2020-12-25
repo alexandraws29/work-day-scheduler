ba32b6c1e58a40391c716e6df5ca8dd0ec434996
Alexa <alexandraws29@outlook.com>
2020-12-25 16:59:13 -0700

    added variable and if else for local storage

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,3 +1,13 @@
 var currentDate = moment().format('MMMM Do YYYY');
 var hour = moment().format("HH");
 var timeState = $('.current-time');
+
+var loadedTasks = localStorage.getItem('task');
+loadedTasks = JSON.parse(loadedTasks);
+if (loadedTasks === null) {
+    console.log("No existing tasks")
+} else {
+    $(".text-box").each(function (index) {
+        $(this).text(loadedTasks[index])
+})}
+
